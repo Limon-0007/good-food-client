eac10ecccebf17c463360358971f3e8e5bac7099
Limon-0007 <limonahmed3023@gmail.com>
2023-05-02 04:38:26 +0600

    added register page

--- src/Components/LoginLayout/Register/Register.jsx
+++ src/Components/LoginLayout/Register/Register.jsx
@@ -1,10 +1,52 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
-            <h2>This is Register page</h2>
+        <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content">
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <div className="card-body">
+                {/* name */}
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input type="text" name='name' placeholder="username" className="input input-bordered" />
+              </div>
+                {/* photo url*/}
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Photo URL</span>
+                </label>
+                <input type="text" name='photo' placeholder="Photo URL" className="input input-bordered" />
+              </div>
+              {/* email */}
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input type="email" name='email' placeholder="email" className="input input-bordered" />
+              </div>
+              {/* password */}
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input type="password" name='password' placeholder="password" className="input input-bordered" />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                </label>
+              </div>
+              {/* button */}
+              <div className="form-control mt-4">
+                <button className="btn btn-primary">Register</button>
+              </div>
+              <p className='font-semibold mt-2'><small>Already have an account? please </small><Link to="/login" className='text-orange-700 underline'>Login</Link></p>
+            </div>
+          </div>
         </div>
+      </div>
     );
 };
 
