a468ba7bd745e793806035bd45e4c7c9ac4d4833
Limon-0007 <limonahmed3023@gmail.com>
2023-05-02 04:32:06 +0600

    added login page

--- src/Components/LoginLayout/Login/Login.jsx
+++ src/Components/LoginLayout/Login/Login.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Login = () => {
     return (
         <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="hero-content">
           <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
             <div className="card-body">
               <div className="form-control">
@@ -22,7 +22,7 @@ const Login = () => {
                   <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                 </label>
               </div>
-              <div className="form-control mt-6">
+              <div className="form-control mt-4">
                 <button className="btn btn-primary">Login</button>
               </div>
               <p className='font-semibold mt-2'><small>Don't have an account? please </small><Link to="/register" className='text-orange-700 underline'>Register</Link></p>
